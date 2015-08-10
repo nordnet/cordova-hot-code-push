4ab7d177a81e6f454df0e55f9fd0b71db8c7df42
Nikolay Demyankov <nikolay.demyankov@nordnet.se>
2015-08-10 14:40:37 +0200

    Added MD5 hash calculation category

---
+++ src/ios/Categories/NSData+MD5.h
@@ -0,0 +1,15 @@
+//
+//  NSData+MD5.h
+//  TestIosCHCP
+//
+//  Created by Nikolay Demyankov on 10.08.15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (MD5)
+
+- (NSString *)md5;
+
+@end

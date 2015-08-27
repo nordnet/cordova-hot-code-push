ab77a83a320baaa732d4db204e10cae92fad0a1e
Nikolay Demyankov <nikolay.demyankov@nordnet.se>
2015-08-27 15:20:11 +0200

    Added some code comments

--- src/android/src/com/nordnetab/chcp/config/ChcpXmlConfigParser.java
+++ src/android/src/com/nordnetab/chcp/config/ChcpXmlConfigParser.java
@@ -4,22 +4,27 @@ import android.content.Context;
 
 import org.apache.cordova.ConfigXmlParser;
 import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-
-import java.io.IOException;
 
 /**
  * Created by Nikolay Demyankov on 06.08.15.
+ *
+ * XML parser for Cordova's config.xml.
+ * Used to read plugin specific preferences.
+ *
+ * @see ChcpXmlConfig
  */
-
 class ChcpXmlConfigParser extends ConfigXmlParser {
 
+    // plugin specific xml keys and attributes in config.xml
     private static class Keys {
+
         public static final String MAIN_TAG = "chcp";
 
+        // keys for application config file url
         public static final String CONFIG_FILE_TAG = "config-file";
         public static final String CONFIG_FILE_URL_ATTRIBUTE = "url";
 
+        // keys for local development
         public static final String LOCAL_DEVELOPMENT_TAG = "local-development";
         public static final String LOCAL_DEVELOPMENT_ENABLED_ATTRIBUTE = "enabled";
     }
@@ -29,13 +34,21 @@ class ChcpXmlConfigParser extends ConfigXmlParser {
     private boolean isInsideChcpBlock;
     private boolean didParseChcpBlock;
 
-    public void parse(Context action, ChcpXmlConfig chcpConfig) {
+    /**
+     * Parse config.xml.
+     * Result is set into passed ChcpXmlConfig instance.
+     *
+     * @param context current context
+     * @param chcpConfig config instance to which we will set preferences from config.xml
+     * @see ChcpXmlConfig
+     */
+    public void parse(Context context, ChcpXmlConfig chcpConfig) {
         this.chcpConfig = chcpConfig;
 
         isInsideChcpBlock = false;
         didParseChcpBlock = false;
 
-        super.parse(action);
+        super.parse(context);
     }
 
     @Override

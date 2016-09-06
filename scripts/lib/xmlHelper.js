9eb8644e987c56431f66d5f9536aa870c21e0603
Nikolay Demyankov <nikolay.demyankov@nordnet.se>
2016-09-06 15:19:07 +0200

    Minor cleanup in the hooks.

--- scripts/lib/xmlHelper.js
+++ scripts/lib/xmlHelper.js
@@ -1,73 +1,70 @@
 /*
 Small helper class to read/write from/to xml file.
 */
-(function() {
 
-  var fs = require('fs'),
-    xml2jsProcessors = require('xml2js/lib/processors'),
-    xml2js = require('xml2js');
 
-  module.exports = {
-    readXmlAsJson: readXmlAsJson,
-    writeJsonAsXml: writeJsonAsXml
-  };
+var fs = require('fs');
+var xml2jsProcessors = require('xml2js/lib/processors');
+var xml2js = require('xml2js');
 
-  /**
-   * Read data from the xml file as JSON object.
-   *
-   * @param {String} filePath - absolute path to xml file
-   * @param {Boolean} simplify - if set to true - cleanup resulting json
-   * @return {Object} JSON object with the contents of the xml file
-   */
-  function readXmlAsJson(filePath, simplify) {
-    var xmlData,
-      xmlParser,
-      parsedData,
-      parserOptions = {};
+module.exports = {
+  readXmlAsJson: readXmlAsJson,
+  writeJsonAsXml: writeJsonAsXml
+};
 
-    if (simplify) {
-      parserOptions = {
-          attrValueProcessors: [xml2jsProcessors.parseNumbers, xml2jsProcessors.parseBooleans],
-          explicitArray: false,
-          mergeAttrs: true,
-          explicitRoot: false
-      };
-    }
+/**
+ * Read data from the xml file as JSON object.
+ *
+ * @param {String} filePath - absolute path to xml file
+ * @param {Boolean} simplify - if set to true - cleanup resulting json
+ * @return {Object} JSON object with the contents of the xml file
+ */
+function readXmlAsJson(filePath, simplify) {
+  var xmlData;
+  var parsedData;
+  var parserOptions = {};
 
-    xmlParser = new xml2js.Parser(parserOptions);
-    try {
-      xmlData = fs.readFileSync(filePath);
-      xmlParser.parseString(xmlData, function(err, data) {
-        if (data) {
-          parsedData = data;
-        }
-      });
-    } catch (err) {}
-
-    return parsedData;
+  if (simplify) {
+    parserOptions = {
+      attrValueProcessors: [xml2jsProcessors.parseNumbers, xml2jsProcessors.parseBooleans],
+      explicitArray: false,
+      mergeAttrs: true,
+      explicitRoot: false
+    };
   }
 
-  /**
-   * Write JSON object as xml into the specified file.
-   *
-   * @param {Object} jsData - JSON object to write
-   * @param {String} filePath - path to the xml file where data should be saved
-   * @param {Object} options - xml options
-   * @return {boolean} true - if data saved to file; false - otherwise
-   */
-  function writeJsonAsXml(jsData, filePath, options) {
-    var xmlBuilder = new xml2js.Builder(options),
-      changedXmlData = xmlBuilder.buildObject(jsData),
-      isSaved = true;
+  var xmlParser = new xml2js.Parser(parserOptions);
+  try {
+    xmlData = fs.readFileSync(filePath);
+    xmlParser.parseString(xmlData, function(err, data) {
+      if (data) {
+        parsedData = data;
+      }
+    });
+  } catch (err) {}
 
-    try {
-      fs.writeFileSync(filePath, changedXmlData);
-    } catch (err) {
-      console.log(err);
-      isSaved = false;
-    }
+  return parsedData;
+}
 
-    return isSaved;
+/**
+ * Write JSON object as xml into the specified file.
+ *
+ * @param {Object} jsData - JSON object to write
+ * @param {String} filePath - path to the xml file where data should be saved
+ * @param {Object} options - xml options
+ * @return {boolean} true - if data saved to file; false - otherwise
+ */
+function writeJsonAsXml(jsData, filePath, options) {
+  var xmlBuilder = new xml2js.Builder(options);
+  var changedXmlData = xmlBuilder.buildObject(jsData);
+  var isSaved = true;
+
+  try {
+    fs.writeFileSync(filePath, changedXmlData);
+  } catch (err) {
+    console.log(err);
+    isSaved = false;
   }
 
-})();
+  return isSaved;
+}

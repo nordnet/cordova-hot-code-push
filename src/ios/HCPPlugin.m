7caf8587fc8a585c0fa130add2d668b300c62d56
Béla Sal <salb3la@gmail.com>
2017-05-10 17:39:24 +0200

    wkwebview-engine-localhost-fix
    With these changes the cordova-hot-code-push plugin works with the
    wkwebview-engine-localhost plugin

--- src/ios/HCPPlugin.m
+++ src/ios/HCPPlugin.m
@@ -318,7 +318,8 @@ static NSString *const DEFAULT_STARTING_PAGE = @"index.html";
     [configParser setDelegate:((id <NSXMLParserDelegate>)delegate)];
     [configParser parse];
     
-    if (delegate.startPage) {
+	//if it isnt a html file fall back to deafult starting page
+     if (delegate.startPage && [delegate.startPage containsString:@".html"]) {
         _indexPage = delegate.startPage;
     } else {
         _indexPage = DEFAULT_STARTING_PAGE;
--- src/ios/WKWebViewFix/CDVWKWebViewEngine+HCPPlugin_ReadAccessURL.m
+++ src/ios/WKWebViewFix/CDVWKWebViewEngine+HCPPlugin_ReadAccessURL.m
@@ -17,34 +17,63 @@
 - (id)loadRequest:(NSURLRequest*)request
 {
     if ([self canLoadRequest:request]) { // can load, differentiate between file urls and other schemes
-        if (request.URL.fileURL) {
-            SEL wk_sel = NSSelectorFromString(CDV_WKWEBVIEW_FILE_URL_LOAD_SELECTOR);
-            
-            // by default we set allowingReadAccessToURL property to the plugin's root folder,
-            // so the WKWebView would load our updates from it.
-            NSURL* readAccessUrl = [HCPFilesStructure pluginRootFolder];
-            
-            // if we are loading index page from the bundle - we need to go up in the folder structure, so the next load from the external storage would work
-            if (![request.URL.absoluteString containsString:readAccessUrl.absoluteString]) {
-                readAccessUrl = [[[request.URL URLByDeletingLastPathComponent] URLByDeletingLastPathComponent] URLByDeletingLastPathComponent];
-            }
-            
-            return ((id (*)(id, SEL, id, id))objc_msgSend)(self.engineWebView, wk_sel, request.URL, readAccessUrl);
-        } else {
-            return [(WKWebView*)self.engineWebView loadRequest:request];
-        }
+		CDVViewController* vc = (CDVViewController*)self.viewController;                
+
+		//if its contains localhost, the application use local web server
+        if([vc.startPage containsString: @"localhost"]) {
+			NSURLComponents *newUrl = [NSURLComponents componentsWithString:vc.startPage];
+
+			//Case: Navigation after update install complete
+			if (request.URL.fileURL) {
+			    newUrl.path = [@"/local-filesystem" stringByAppendingString: request.URL.path];            
+
+			    return [(WKWebView*)self.engineWebView loadRequest:[NSURLRequest requestWithURL:newUrl.URL]];
+			} 
+			//Case: load application start page
+			else {
+				NSURL *wwwPath = [NSURL URLWithString:vc.wwwFolderName];
+			    if(wwwPath.fileURL) {
+			        newUrl.path = [@"/local-filesystem" stringByAppendingString:[wwwPath.path stringByAppendingString:@"/index.html"] ];
+
+			        return [(WKWebView*)self.engineWebView loadRequest:[NSURLRequest requestWithURL:newUrl.URL]];
+			    }
+			    else {
+			        return [(WKWebView*)self.engineWebView loadRequest:request];
+			    }
+			}
+		}
+		//Original way
+		else {
+			if (request.URL.fileURL) {
+			    SEL wk_sel = NSSelectorFromString(CDV_WKWEBVIEW_FILE_URL_LOAD_SELECTOR);
+			    
+			    // by default we set allowingReadAccessToURL property to the plugin's root folder,
+			    // so the WKWebView would load our updates from it.
+			    NSURL* readAccessUrl = [HCPFilesStructure pluginRootFolder];
+			    
+			    // if we are loading index page from the bundle - we need to go up in the folder structure, so the next load from the external storage would work
+			    if (![request.URL.absoluteString containsString:readAccessUrl.absoluteString]) {
+			        readAccessUrl = [[[request.URL URLByDeletingLastPathComponent] URLByDeletingLastPathComponent] URLByDeletingLastPathComponent];
+			    }
+			    
+			    return ((id (*)(id, SEL, id, id))objc_msgSend)(self.engineWebView, wk_sel, request.URL, readAccessUrl);
+			} else {
+			    return [(WKWebView*)self.engineWebView loadRequest:request];
+			}
+		}
     } else { // can't load, print out error
         NSString* errorHtml = [NSString stringWithFormat:
-                               @"<!doctype html>"
-                               @"<title>Error</title>"
-                               @"<div style='font-size:2em'>"
-                               @"   <p>The WebView engine '%@' is unable to load the request: %@</p>"
-                               @"   <p>Most likely the cause of the error is that the loading of file urls is not supported in iOS %@.</p>"
-                               @"</div>",
-                               NSStringFromClass([self class]),
-                               [request.URL description],
-                               [[UIDevice currentDevice] systemVersion]
+								@"<!doctype html>"
+								@"<title>Error</title>"
+								@"<div style='font-size:2em'>"
+								@"   <p>The WebView engine '%@' is unable to load the request: %@</p>"
+								@"   <p>Most likely the cause of the error is that the loading of file urls is not supported in iOS %@.</p>"
+								@"</div>",							   
+								NSStringFromClass([self class]),
+								[request.URL description],
+								[[UIDevice currentDevice] systemVersion]
                                ];
+
         return [self loadHTMLString:errorHtml baseURL:nil];
     }
 }

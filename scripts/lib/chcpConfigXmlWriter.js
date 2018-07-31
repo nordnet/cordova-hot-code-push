e7994c19fbd65e84da72fc92d37a42f9680c15e2
ktojong <ktojong@gmail.com>
2018-07-31 14:25:11 +0900

    cordova android 7.x+ 대응

--- scripts/lib/chcpConfigXmlWriter.js
+++ scripts/lib/chcpConfigXmlWriter.js
@@ -1,7 +1,7 @@
 /**
-Helper module to work with config.xml file.
-We will use it to inject plugin-specific options.
-*/
+ Helper module to work with config.xml file.
+ We will use it to inject plugin-specific options.
+ */
 
 
 var path = require('path');
@@ -82,6 +82,9 @@ function pathToIosConfigXml() {
  * @return {String} absolute path to config.xml file
  */
 function pathToAndroidConfigXml() {
+  if (cordovaContext.opts.cordova.version >= "7.0.0") {
+    return path.join(projectRoot, 'platforms', 'android', 'app', 'src', 'main', 'res', 'xml', 'config.xml');
+  }
   return path.join(projectRoot, 'platforms', 'android', 'res', 'xml', 'config.xml');
 }
 
@@ -94,16 +97,14 @@ function pathToAndroidConfigXml() {
 function getPlatformSpecificConfigXml(platform) {
   var configFilePath = null;
   switch (platform) {
-    case 'ios':
-      {
-        configFilePath = pathToIosConfigXml();
-        break;
-      }
-    case 'android':
-      {
-        configFilePath = pathToAndroidConfigXml();
-        break;
-      }
+    case 'ios': {
+      configFilePath = pathToIosConfigXml();
+      break;
+    }
+    case 'android': {
+      configFilePath = pathToAndroidConfigXml();
+      break;
+    }
   }
 
   return configFilePath;
@@ -115,7 +116,7 @@ function getPlatformSpecificConfigXml(platform) {
  * @param {Object} options - plugin options
  */
 function injectOptions(options) {
-  platforms.forEach(function(platform) {
+  platforms.forEach(function (platform) {
     var configXmlFilePath = getPlatformSpecificConfigXml(platform);
     if (configXmlFilePath == null) {
       return;
